3b5fe8ac1501cdea52ce053d8960f69361284485
MarkZither <mark.burton@zither-it.co.uk>
2022-10-01 21:11:37 +0200

    add bin to package.json for multiple commands

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+#!/usr/bin/env node
 // index.js
-console.log("Hello world");
+var temat = "Fwd: Lorum Ipsum PROD dolor sit amet, consectetur adipiscing elit [...] [TEST-TICKET#2022092817133275]"; //workflowContext.trigger.outputs.body.Subject
+var lastopensquarebracket = temat.lastIndexOf('[');
+var ticketId = temat.substring(lastopensquarebracket);
+
+
+console.log(ticketId);
